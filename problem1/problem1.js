7f9272de63629724c05a82433e35849b39c19ce2
Luc Mosser <luc.mosser86@orange.fr>
2020-01-06 09:11:07 +0100

    1st challenge

---
+++ problem1/problem1.js
@@ -0,0 +1,10 @@
+function multiplesOf3and5(number) {
+let total = 0;
+
+  for(let i = 0; i <= number; i++) {
+    if(i % 3 == 0 || i % 5 == 0) {
+      total += i;
+    }
+  }
+  console.log(total)
+}
