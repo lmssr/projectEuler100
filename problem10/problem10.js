05bea3e0cd29a6dcbac22b41c21a72a5b2f49327
Luc Mosser <luc.mosser86@orange.fr>
2020-01-14 18:19:25 +0100

    almost working

--- problem10/problem10.js
+++ problem10/problem10.js
@@ -1,6 +1,22 @@
+
 function primeSummation(n) {
-  // Good luck!
-  return true;
+  let arr = [];
+  for(let i = 2; i < n; i++) {
+    for(let j = 2; j <= i; j++) {
+      if(i===j){
+        arr.push(i)
+      }
+      if(i%j===0) {
+        break
+      }
+    }
+  }
+  return arr.reduce(function(x,y){return x+y})
 }
+
+
+primeSummation(2000000);
+
+
 
 primeSummation(2000000);
